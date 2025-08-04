86496e38277eabd489867d781710861e946d6afd
Astronaute <kemalguersoy@gmail.com>
2025-08-04 09:50:56 +0300

    Update portfolioService.js

--- src/portfolioService.js
+++ src/portfolioService.js
@@ -1,20 +1,21 @@
 
-import { ethers } from 'ethers';
+import { BrowserProvider } from 'ethers';
+import { formatEther } from 'ethers';
 
 const COINGECKO_API_KEY = process.env.REACT_APP_COINGECKO_API_KEY;
 const COINGECKO_API = 'https://api.coingecko.com/api/v3';
 
 /**
- * Fetch ETH balance from blockchain
+ * Fetch ETH balance using ethers v6+ via MetaMask
  */
 export async function getEthBalance(address) {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const provider = new BrowserProvider(window.ethereum);
   const balance = await provider.getBalance(address);
-  return ethers.utils.formatEther(balance);
+  return formatEther(balance);
 }
 
 /**
- * Fetch current ETH price in USD from Coingecko
+ * Fetch ETH price in USD from Coingecko
  */
 export async function getEthPrice() {
   const res = await fetch(`${COINGECKO_API}/simple/price?ids=ethereum&vs_currencies=usd&x_cg_pro_api_key=${COINGECKO_API_KEY}`);
@@ -23,7 +24,7 @@ export async function getEthPrice() {
 }
 
 /**
- * Get full portfolio summary (ETH + USD value)
+ * Return full ETH portfolio snapshot
  */
 export async function getPortfolio(address) {
   const ethBalance = await getEthBalance(address);
